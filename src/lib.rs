fc6fe047056e15d84a065f611fd38a011e26712c
Paulo Vidal <u1d4lp@gmail.com>
2022-09-21 22:51:34 -0300

    scaffold get args fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,26 @@
 use std::error::Error;
+use clap::{App, Arg} 
 
-type Result<T> = Result<T, Box<dyn Error>>;
+type OpsResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank: bool,
+}
+
+pub fn get_args() -> OpsResult<Config> {
+    let matches = App::new("chat")
+        .version("0.1.0")
+        .author("Paul Vidal <u1d4lp@gmail.com>")
+        .about("A simple cat clone built in Rust")
+        //
+        .get_matches();
+
+        Ok(Config {
+            files: ...,
+            number_lines: ...,
+            number_nonblank: ...,
+        })
 }
