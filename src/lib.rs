2dc2a0218d8b646d70214d9d9ac47f9232d31c03
Paulo Vidal <u1d4lp@gmail.com>
2022-09-21 23:02:13 -0300

    impls args matches

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+use clap::{App, Arg};
 use std::error::Error;
-use clap::{App, Arg} 
 
 type OpsResult<T> = Result<T, Box<dyn Error>>;
 
@@ -20,12 +20,33 @@ pub fn get_args() -> OpsResult<Config> {
         .version("0.1.0")
         .author("Paul Vidal <u1d4lp@gmail.com>")
         .about("A simple cat clone built in Rust")
-        //
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number_lines")
+                .short("n")
+                .long("number-lines")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number nonblank lines")
+                .takes_value(false),
+        )
         .get_matches();
 
-        Ok(Config {
-            files: ...,
-            number_lines: ...,
-            number_nonblank: ...,
-        })
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank: matches.is_present("number_nonblank"),
+    })
 }
